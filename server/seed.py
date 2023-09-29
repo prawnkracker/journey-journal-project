4545ce3aad127d824c730f6d3d49347d4926e30c
Dennis Demir <dennisjdemir@gmail.com>
2023-09-29 14:11:55 -0400

    add seed code for user model

--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,40 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, User, Review, Trip
 
 if __name__ == '__main__':
+    
     fake = Faker()
+
     with app.app_context():
         print("Starting seed...")
         # Seed code goes here!
+        print("Deleting all records...")
+        User.query.delete()
+        Review.query.delete()
+        Trip.query.delete()
+
+
+        print("Creating users...")
+        # to create unique usernames
+        users=[]
+        usernames=[]
+
+        for _ in range(10):
+            
+            username = fake.simple_profile()['username']
+            while username in usernames:
+                username = fake.simple_profile()['username']
+            usernames.append(username)
+
+            user = User(
+                username=username,
+                bio=fake.paragraph(nb_sentences=2),
+                image_url=fake.image_url(width=100, height=100)
+            )
+
+            user.password_hash = user.username + 'pass'
+
+            users.append(user)
+        db.session.add_all(users)
