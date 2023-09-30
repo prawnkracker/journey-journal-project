10303ee622081cafaa7f9894d4f4866b1e9b9c18
Dennis Demir <dennisjdemir@gmail.com>
2023-09-30 02:31:24 -0400

    add signup resource

--- server/app.py
+++ server/app.py
@@ -3,19 +3,34 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, session, make_response
 from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
 
 # Local imports
 from config import app, db, api
 # Add your model imports
-from models import User, Review
+from models import User, Review, Trip
 
 # Views go here!
 
-@app.route('/')
-def index():
-    return '<h1>Project Server</h1>'
+class Signup(Resource):
+    def post(self):
+        json = request.get_json()
+        user = User(
+            username=json.get('username'),
+            bio=json.get('bio'),
+            image_url=json.get('image_url')
+        )
+        user.password_hash=json.get('password')
+        try:
+            db.session.add(user)
+            db.session.commit()
+            session['user_id']= user.id
+            return user.to_dict(), 201
+        except IntegrityError as e:
+            db.session.rollback()
+            return {"message":"Unprocessable entity."}, 422
 
 
 if __name__ == '__main__':
