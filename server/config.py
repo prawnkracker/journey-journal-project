6e5a9b59a46d6cfbe6d37f0a9c388491501a1b3d
Dennis Demir <dennisjdemir@gmail.com>
2023-09-30 15:57:36 -0400

    add env and secret key to app

--- server/config.py
+++ server/config.py
@@ -1,6 +1,7 @@
 # Standard library imports
 
 # Remote library imports
+import os
 from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
@@ -8,11 +9,14 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv
+load_dotenv()
 
 # Local imports
 
 # Instantiate app, set attributes
 app = Flask(__name__)
+app.secret_key=os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
