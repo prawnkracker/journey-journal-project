62fb5996db815bf482c6383d2e2b6a95decfefc3
Dennis Demir <dennisjdemir@gmail.com>
2023-10-03 01:10:18 -0400

    add homepage component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import Home from './Home'
 
 function App() {
-  return <h1>Project Client</h1>;
+  return (
+    <div className="app">
+      <Route exact path='/'>
+        <Home />
+      </Route>
+    </div>
+  )
 }
 
 export default App;
---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home(){
+    return (
+        <div className='homepage'>
+            <h1>Welcome to Journey Journal</h1>
+            <h2>Where you can find new places to visit or add some of your trip experiences.</h2>
+            <img src='https://i.etsystatic.com/7134734/r/il/5dc567/2035990985/il_570xN.2035990985_cm78.jpg' alt='Globe'/>
+        </div>
+    )
+}
+
+export default Home;
