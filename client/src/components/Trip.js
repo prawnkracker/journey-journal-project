3bad28522ff460ceae7ce8801c13f28a08b28021
Dennis Demir <dennisjdemir@gmail.com>
2023-10-04 13:51:13 -0400

    build basic trip card component

---
+++ client/src/components/Trip.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Trip({trip}){
+    const {destination, approximate_cost, description, trip_image_url} = trip
+    return (
+        <div className="trip-card">
+            <h1>{destination}</h1>
+            <img src={trip_image_url} />
+            <h2>${approximate_cost}</h2>
+            <p><b>Description:</b> {description}</p>
+        </div>
+    )
+}
+
+export default Trip;
