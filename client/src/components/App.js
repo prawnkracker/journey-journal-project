676e5850e7ac6381a51d183fd0f0133bdb5d43f1
Dennis Demir <dennisjdemir@gmail.com>
2023-10-04 13:50:56 -0400

    add fetch to trips index resource to get trips

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from './Home'
+import TripList from './TripList'
 
 function App() {
+  const [trips, setTrips] = useState([])
+  
+  useEffect(() => {
+    fetch('/trips_index')
+    .then((r) => r.json())
+    .then((data) => setTrips(data))
+  }, []);
+  console.log(trips)
+  
   return (
     <div className="app">
-      <Route exact path='/'>
-        <Home />
-      </Route>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/triplist'>
+          <TripList trips={trips}/>
+        </Route>
+      </Switch>
     </div>
   )
 }
