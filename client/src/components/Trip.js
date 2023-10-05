532973b6c07a5dede2d13c58350cc72a9550e674
Dennis Demir <dennisjdemir@gmail.com>
2023-10-05 03:44:04 -0400

    add button link to see trip and its reviews

--- client/src/components/Trip.js
+++ client/src/components/Trip.js
@@ -1,13 +1,19 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 function Trip({trip}){
-    const {destination, approximate_cost, description, trip_image_url} = trip
+    const {id, destination, approximate_cost, description, trip_image_url} = trip
+
+    
     return (
         <div className="trip-card">
             <h1>{destination}</h1>
-            <img src={trip_image_url} alt='Picture of destination'/>
+            <img src={trip_image_url} alt='Destination'/>
             <h2>${approximate_cost}</h2>
             <p><b>Description:</b> {description}</p>
+            <Link to={`/tripreviews/${id}`}>
+                <button>See more</button>
+            </Link>
         </div>
     )
 }
