770d05db7ecddc8e63b819a7b7ee2f389bf520eb
Dennis Demir <dennisjdemir@gmail.com>
2023-10-06 01:21:52 -0400

    add return if a trip has no reviews yet

--- client/src/components/Review.js
+++ client/src/components/Review.js
@@ -13,12 +13,18 @@ function Review({reviewData}){
     console.log(review, date_created.slice(0, 10))
 
     return(
-        <div className="review_card">
-            <h3>User: {user.username}</h3>
-            <p>{review}</p>
-            <p><b>Date Created:</b> {date_created.slice(0,10)}</p>
-            <br></br>
-        </div>
+        <>
+        {review ? 
+                (<div className="review_card">
+                    <h3>User: {user.username}</h3>
+                    <p>{review}</p>
+                    <p><b>Date Created:</b> {date_created.slice(0,10)}</p>
+                </div>) 
+            :  
+            (<div className="review_card">
+                <h2>No reviews yet!</h2>
+            </div>)}
+        </>
     )
 }
 
