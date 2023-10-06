10f7f9eb39d864d7c9043c98610cf91df06663ec
Dennis Demir <dennisjdemir@gmail.com>
2023-10-06 19:48:13 -0400

    add edit review button for user that is logged in

--- client/src/components/Review.js
+++ client/src/components/Review.js
@@ -1,7 +1,7 @@
 import React, { useState,useEffect } from "react";
 import { Link } from "react-router-dom";
 
-function Review({reviewData}){
+function Review({reviewData, currentUser}){
     const {review, date_created, user_id, trip_id} = reviewData
     const [user, setUser] = useState({})
     const [trip, setTrip] = useState({})
@@ -16,16 +16,19 @@ function Review({reviewData}){
         .then((data) => setTrip(data))
     }, [user_id, trip_id])
 
-        return (
-            <div className="review_card">
-                <h3>User:</h3> 
-                <Link to={`/userreviews/${user_id}`}>
-                    {user.username}
-                </Link>
-                <p>{review}</p>
-                <p><b>Date Created:</b> {date_created.slice(0,10)}</p>
-                <p><em>Trip:</em> {trip.destination}</p>
-            </div>)
-    }
+    return (
+        <div className="review-card">
+            <h3>User:</h3> 
+            <Link to={`/userreviews/${user_id}`}>
+                {user.username}
+            </Link>
+            <p>{review}</p>
+            <p><b>Date Created:</b> {date_created.slice(0,10)}</p>
+            <p><em>Trip:</em> {trip.destination}</p>
+            {currentUser.id === user_id && (
+                <button>Edit Review</button>
+            )}
+        </div>)
+}
 
 export default Review;
