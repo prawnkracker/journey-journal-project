bc9873832ea3249a5ca4d71909783507ea9610c8
Dennis Demir <dennisjdemir@gmail.com>
2023-10-07 05:19:51 -0400

    add edit forms to user page

--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -3,26 +3,85 @@ import Review from "./Review";
 
 function UserPage({currentUser, trips}){
     const [reviews, setReviews] = useState([])
+    const [showUserEdit, setShowUserEdit] = useState('none')
+    const [user, setUser] = useState(currentUser)
+    const [form, setForm] = useState({
+        bio:user.bio,
+        image_url:user.image_url,
+    })
     
     useEffect(() => {
-        fetch(`/${currentUser.id}/reviews`)
+        fetch(`/${user.id}/reviews`)
         .then((r) => r.json())
         .then((reviews) => setReviews(reviews))
 
     }, [])
 
+    function handleChange(e){
+        const {name, value} = e.target
+
+        setForm({
+            ...form,
+            [name]:value
+        })
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        const userInfo={
+            bio:form.bio,
+            image_url:form.bio
+        }
+        fetch(`/user/${user.id}`, {
+            method:"PATCH",
+            headers:{"Content-Type":"application/json"},
+            body: JSON.stringify(userInfo)
+        })
+        .then((r) => r.json())
+        .then((data) => setUser(data))
+
+        setForm({
+            bio:user.bio,
+            image_url:user.image_url
+        })
+        setShowUserEdit('none')
+    }
+
     return (
         <div className="user-card">
-        <img src={`${currentUser.image_url}`} alt="User avatar"/>
-        <h1>{currentUser.username}</h1>
-        <p>{currentUser.bio}</p>
+        <img src={`${user.image_url}`} alt="User avatar"/>
+        <h1>{user.username}</h1>
+        <p><b>Bio:</b> {user.bio}</p>
+        <button onClick={() => setShowUserEdit('block')}>Edit User Info</button>
+        <form onSubmit={handleSubmit} style={{display:showUserEdit}}>
+            <h4>New Bio:</h4>
+            <input 
+                type="text"
+                name="bio"
+                id="bio"
+                autoComplete="off"
+                value={form.bio}
+                onChange={handleChange}
+            />
+            <h4>New Image:</h4>
+            <input 
+                type="text"
+                name="image_url"
+                id="image_url"
+                autoComplete="off"
+                value={form.image_url}
+                onChange={handleChange}
+            />
+            <button type="submit">Update User</button>
+            <button onClick={() => setShowUserEdit('none')}>Close</button>
+        </form>
         <h2>Reviews:</h2>
         {reviews.map((review) => {
             return (
             <Review
             key={review.id}
             reviewData={review}
-            currentUser={currentUser}
+            currentUser={user}
             trips={trips}
             />
         )})}
